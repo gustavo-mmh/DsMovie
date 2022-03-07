2e3b48bb5c1a98e1b82c35f81f4c74442bad10d1
Gustavo <gustavo-mmello@hotmail.com>
2022-03-07 19:09:28 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperir/devmovie/DevmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperir.devmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

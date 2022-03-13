497ac120e715a3f0fe53b2a403588f10c64e518d
Gustavo <gustavo-mmello@hotmail.com>
2022-03-13 11:31:02 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,13 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+
+axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+        console.log(response.data);
+    });
+
 function Listing() {
     return (
         <>
